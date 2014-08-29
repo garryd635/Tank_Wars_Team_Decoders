14e43d925d6d526caaf48fd7fb28be46f8978173
Asen52 <52ahmetsen@gmail.com>
2014-08-29 15:43:28 -0400

    First Commit-Uploading the project to GitHub

---
+++ Tank_Wars_Team_Decoders/src/main/dataClassTester.java
@@ -0,0 +1,19 @@
+package main;
+
+
+public class dataClassTester 
+{
+	static DataClass dc = new DataClass();
+	
+	public static void main(String[] args) 
+	{
+		dataClassTester();
+	}
+	
+	public static void dataClassTester()
+	{
+		DataClass.readFile();
+	}
+	
+	
+}
